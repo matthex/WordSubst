7330149f7d7036a2576e353f601ab0b858c2ae25
Matthias Koch <matthex.koch@googlemail.com>
2021-08-01 17:37:27 +0200

    Add project files.

---
+++ WordSubst/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿using System.IO;
+namespace WordSubst
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.templateLabel = new System.Windows.Forms.Label();
+            this.templatePathLabel = new System.Windows.Forms.Label();
+            this.substPanel = new System.Windows.Forms.Panel();
+            this.generateButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // templateLabel
+            // 
+            this.templateLabel.AutoSize = true;
+            this.templateLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.templateLabel.Location = new System.Drawing.Point(13, 13);
+            this.templateLabel.Name = "templateLabel";
+            this.templateLabel.Size = new System.Drawing.Size(59, 13);
+            this.templateLabel.TabIndex = 0;
+            this.templateLabel.Text = "Template";
+            this.templateLabel.Click += new System.EventHandler(this.template_Click);
+            // 
+            // templatePathLabel
+            // 
+            this.templatePathLabel.AutoSize = true;
+            this.templatePathLabel.Location = new System.Drawing.Point(78, 13);
+            this.templatePathLabel.Name = "templatePathLabel";
+            this.templatePathLabel.Size = new System.Drawing.Size(113, 13);
+            this.templatePathLabel.TabIndex = 1;
+            this.templatePathLabel.Text = "No templated selected";
+            this.templatePathLabel.Click += new System.EventHandler(this.template_Click);
+            // 
+            // substPanel
+            // 
+            this.substPanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.substPanel.AutoScroll = true;
+            this.substPanel.Location = new System.Drawing.Point(12, 29);
+            this.substPanel.Name = "substPanel";
+            this.substPanel.Size = new System.Drawing.Size(430, 191);
+            this.substPanel.TabIndex = 2;
+            // 
+            // generateButton
+            // 
+            this.generateButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.generateButton.Location = new System.Drawing.Point(182, 226);
+            this.generateButton.Name = "generateButton";
+            this.generateButton.Size = new System.Drawing.Size(75, 23);
+            this.generateButton.TabIndex = 3;
+            this.generateButton.Text = "Generate";
+            this.generateButton.UseVisualStyleBackColor = true;
+            this.generateButton.Click += new System.EventHandler(this.generateDoc);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(454, 261);
+            this.Controls.Add(this.generateButton);
+            this.Controls.Add(this.substPanel);
+            this.Controls.Add(this.templatePathLabel);
+            this.Controls.Add(this.templateLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "WordSubst";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label templateLabel;
+        private System.Windows.Forms.Label templatePathLabel;
+        private System.Windows.Forms.Panel substPanel;
+        private System.Windows.Forms.Button generateButton;
+    }
+}
+
---
+++ WordSubst/Form1.cs
@@ -0,0 +1,152 @@
+﻿using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace WordSubst
+{
+    public partial class Form1 : Form
+    {
+        HashSet<String> templateFields;
+        String selectedTemplatePath;
+
+        public Form1()
+        {
+            InitializeComponent();
+            Properties.Settings.Default.SettingChanging += new System.Configuration.SettingChangingEventHandler(SettingChanging);
+
+            if (!(Properties.Settings.Default.selectedTemplatePath.Equals("")) && File.Exists(Properties.Settings.Default.selectedTemplatePath))
+            {
+                this.selectedTemplatePath = Properties.Settings.Default.selectedTemplatePath;
+                templatePathLabel.Text = Path.GetFileNameWithoutExtension(this.selectedTemplatePath);
+                getTemplateFields();
+                populateSubstPanel(this.templateFields);
+            }
+        }
+
+        public static string GetPropertyName<T>(Expression<Func<T>> expression)
+        {
+            MemberExpression body = (MemberExpression)expression.Body;
+            return body.Member.Name;
+        }
+
+        private void template_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fileDlg = new OpenFileDialog();
+            fileDlg.Filter = "Word Documents|*.docx";
+            if (!Properties.Settings.Default.selectedTemplatePath.Equals(""))
+            {
+                fileDlg.InitialDirectory = Path.GetDirectoryName(this.selectedTemplatePath);
+            }
+            if (fileDlg.ShowDialog() == DialogResult.OK)
+            {
+                this.selectedTemplatePath = fileDlg.FileName;
+                Properties.Settings.Default.selectedTemplatePath = fileDlg.FileName;
+                Properties.Settings.Default.Save();
+            }
+        }
+
+        void SettingChanging(object sender, System.Configuration.SettingChangingEventArgs e)
+        {
+            if(e.SettingName.Equals(GetPropertyName(() => Properties.Settings.Default.selectedTemplatePath)))
+            {
+                this.selectedTemplatePath = e.NewValue.ToString();
+                templatePathLabel.Text = Path.GetFileNameWithoutExtension(e.NewValue.ToString());
+            }
+            getTemplateFields();
+            populateSubstPanel(this.templateFields);
+        }
+
+        void getTemplateFields()
+        {
+            HashSet<String> templateFields = new HashSet<String>();
+
+            Regex pattern = new Regex("@@@((.)*?)###", RegexOptions.Compiled);
+
+            //TODO: Excpetion handling: file in use
+            using (WordprocessingDocument wordDoc = WordprocessingDocument.Open(this.selectedTemplatePath, true))
+            {
+                string docText = null;
+                using (StreamReader sr = new StreamReader(wordDoc.MainDocumentPart.GetStream()))
+                {
+                    docText = sr.ReadToEnd();
+                }
+
+                MatchCollection matches = pattern.Matches(docText);
+                foreach (Match match in matches)
+                {
+                    if (match.Success)
+                    {
+                        templateFields.Add(match.Groups[1].Value);
+                    }
+                }
+            }
+
+            this.templateFields = templateFields;
+        }
+
+        void populateSubstPanel(HashSet<string> templateFields)
+        {
+            substPanel.Controls.Clear();
+
+            int i = 1;
+            foreach (String templateField in templateFields)
+            {
+                Label label = new Label();
+                label.Text = templateField;
+                label.Name = templateField + "Label";
+                label.Size = new Size(150, 20);
+                label.Location = new Point(10, i * 25);
+
+                TextBox textbox = new TextBox();
+                textbox.Name = templateField + "Textbox";
+                textbox.Size = new Size(250, 90);
+                textbox.Location = new Point(160, i * 25 - 2);
+
+                substPanel.Controls.Add(label);
+                substPanel.Controls.Add(textbox);
+                i++;
+            }
+        }
+
+        private void generateDoc(object sender, EventArgs e)
+        {
+            String newFilePath = Path.GetDirectoryName(this.selectedTemplatePath) + "/" + "New" + DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss") + Path.GetFileNameWithoutExtension(this.selectedTemplatePath) + Path.GetExtension(this.selectedTemplatePath);
+            File.Copy(this.selectedTemplatePath, newFilePath);
+            using (WordprocessingDocument wordDoc = WordprocessingDocument.Open(newFilePath, true))
+            {
+                string docText = null;
+                using (StreamReader sr = new StreamReader(wordDoc.MainDocumentPart.GetStream()))
+                {
+                    docText = sr.ReadToEnd();
+                }
+
+                foreach (String templateField in this.templateFields)
+                {
+                    string newValue = ((TextBox)substPanel.Controls.Find(templateField + "Textbox", true)[0]).Text;
+                    Regex pattern = new Regex("@@@" + templateField + "###", RegexOptions.Compiled);
+                    docText = pattern.Replace(docText, newValue);
+                }
+
+                using (StreamWriter sw = new StreamWriter(wordDoc.MainDocumentPart.GetStream(FileMode.Create)))
+                {
+                    sw.Write(docText);
+                }
+            }
+            //ProcessStartInfo startInfo = new ProcessStartInfo();
+            //startInfo.FileName = "WINWORD.EXE";
+            //startInfo.Arguments = newFilePath;
+            //Process.Start(startInfo);
+        }
+    }
+}
